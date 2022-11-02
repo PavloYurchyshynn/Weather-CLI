8b7c28910fd005d133d64101d5d9db3fc25ff0bd
PavloYurchyshynn <gamepaha123@gmail.com>
2022-11-02 15:10:26 +0200

    feat: finish app

---
+++ services/log.service.js
@@ -0,0 +1,34 @@
+import chalk from 'chalk'
+import dedent from 'dedent-js'
+
+const printError = (error) => {
+    console.log(chalk.bgRed(' ERROR ') + ' ' + error);
+}
+
+const printSuccess = (message) => {
+    console.log(chalk.bgGreen(' SUCCESS ') + ' ' + message);
+}
+
+const printHelp = () => {
+    console.log(
+        dedent`${chalk.bgCyan(' HELP ')}
+        Без параментів - вивід погоди
+        -s [CITY] для установки міста
+        -h для виводу допомоги
+        -t [API_KEY] для збереження токена
+        `
+    );
+}
+
+const printWeather = (res, icon) => {
+    console.log(
+        dedent`${chalk.bgYellow(' WEATHER ')} Погода в місті ${res.name}
+        ${icon}  ${res.weather[0].description}
+        Температура: ${res.main.temp} (Відчувається як ${res.main.feels_like})
+        Волога: ${res.main.humidity}%
+        Швидкість вітра: ${res.wind.speed}
+        `
+    );
+}
+
+export { printError, printSuccess, printHelp, printWeather }
